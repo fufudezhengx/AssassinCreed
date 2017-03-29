2f7dfb10d5a673693a55d062878086b77463fef6
Jack <fufudezhengx@gmail.com>
2017-03-29 14:40:45 +0800

    add redirect and flasash

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,session,redirect,url_for,flash
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
@@ -22,12 +22,14 @@ app.config['SECRET_KEY'] = 'ASDF'
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html',current_time=datetime.utcnow(),name=name,form=form)
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you change your name')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html',current_time=datetime.utcnow(),name=session.get('name'),form=form)
     
 @app.route('/user/<name>')
 def user(name):
