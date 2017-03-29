4c68f54513db508de27a928f1aa1afa2a9809f65
Jack <fufudezhengx@gmail.com>
2017-03-29 14:15:47 +0800

    add form

--- index.py
+++ index.py
@@ -5,14 +5,29 @@ from flask_moment import Moment
 from datetime import datetime
 
 
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField
+from wtforms.validators import Required
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?',validators = [Required()])
+    submit = SubmitField('Submit')
+
 app = Flask(__name__)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+app.config['SECRET_KEY'] = 'ASDF'
+
+@app.route('/',methods=['GET','POST'])
 def index():
-    return render_template('index.html',current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html',current_time=datetime.utcnow(),name=name,form=form)
     
 @app.route('/user/<name>')
 def user(name):
